b95285d1d2238e0b8b6f21d1fa1cc0bfadea308b
Andy Jenkins <andy@jenkinssd.com>
2024-10-12 13:06:19 -0700

    app layout and working menu selections

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -4,34 +4,47 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { ChatComponent } from './chat/chat.component';
 import { GraphComponent } from './graph/graph.component';
 import { SharedDataService } from './services/shared-data.service';
+import { AngularSplitModule } from 'angular-split';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-app',
   standalone: true,
-  imports: [MatSidenavModule, MatListModule, ChatComponent, GraphComponent],
+  imports: [
+    MatSidenavModule,
+    MatListModule,
+    ChatComponent,
+    GraphComponent,
+    AngularSplitModule,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  selectedConversation: any;
   title = 'my-app';
   sidebarFlex = '0 0 10%';
   graphData: any;
   chatHistory: any;
+  isSidenavOpen = true;
+  isGraphView = false;
+  conversations = [];
 
   constructor(private sharedDataService: SharedDataService) {}
 
   ngOnInit(): void {
-    const data = this.sharedDataService.getData();
-    console.log(JSON.stringify(data, null, 2));
-    this.graphData = this.preprocessData(data);
-    this.chatHistory = this.findPath(data, 'Explain unsupervised learning');
+    this.graphData = this.sharedDataService.getData();
+    console.log(JSON.stringify(this.graphData, null, 2));
+    this.conversations = this.preprocessData(this.graphData);
+    this.chatHistory = this.findPath(
+      this.graphData,
+      'Explain unsupervised learning',
+    );
   }
 
   preprocessData(data: any): any {
-    return data.map((item: any) => ({
-      title: item.title,
-      queries: this.preprocessData(item.queries),
-    }));
+    return data.map((item: any) => item.title);
   }
 
   findPath(data: any, targetQuery: string, path: any[] = []): any[] {
@@ -49,5 +62,9 @@ export class AppComponent {
       }
     }
     return [];
+  }
+  selectConversation(conversation: any): void {
+    this.selectedConversation = conversation;
+    this.chatHistory = this.findPath(this.graphData, conversation.query);
   }
 }
--- src/app/app/graph/graph.component.ts
+++ src/app/app/graph/graph.component.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-graph',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatSidenavModule, MatListModule],
   templateUrl: './graph.component.html',
   styleUrl: './graph.component.scss',
 })
 export class GraphComponent {
-  @Input() graphData: any;
+  @Input() item: any;
 }
