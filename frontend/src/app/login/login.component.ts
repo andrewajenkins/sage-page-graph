2900b9e43275c64448e0835269af376d427f003a
Andy Jenkins <andy@jenkinssd.com>
2024-11-28 21:25:12 -0800

    add basic login

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -3,12 +3,20 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
-import { MatInput } from '@angular/material/input';
+import { MatInput, MatInputModule } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatFormField, FormsModule, MatInput, MatFormFieldModule],
+  imports: [
+    MatFormField,
+    FormsModule,
+    MatInput,
+    MatFormFieldModule,
+    MatButton,
+    MatInputModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
@@ -23,6 +31,7 @@ export class LoginComponent {
   ) {}
 
   onSubmit(): void {
+    console.log('submitted');
     this.http
       .post('/api/login/', {
         username: this.username,
@@ -30,10 +39,10 @@ export class LoginComponent {
       })
       .subscribe({
         next: () => {
-          this.router.navigate(['/dashboard']); // Redirect to your app's main page
+          this.router.navigate(['/app']); // Redirect to your app's main page
         },
         error: (err) => {
-          this.errorMessage = 'Invalid username or password';
+          alert('Invalid username or password');
         },
       });
   }
